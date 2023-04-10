bb91c76af6ebcd0a9214ef2bfdb2943fb3b164aa
Enock <bernardoenockgm@gmail.com>
2023-04-09 21:25:58 -0300

    refactor: main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,6 +3,7 @@ module.exports = {
     "../stories/**/*.stories.mdx",
     "../stories/**/*.stories.@(js|jsx|ts|tsx)",
     "../src/**/*.stories.mdx",
+    "../src/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   "addons": [
     {
